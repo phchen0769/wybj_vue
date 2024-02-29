d00a1472e4348f5186d9adfec514d82591085ad0
Fedorov <phchen0769@icloud.com>
2024-02-29 10:49:01 +0800

    项目初始化成功。

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# admin_vue
-后台前端解决方案
+# wybj_vue
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" on-click="@myFunction"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+
+  name: 'HomeView',
+
+  components: { HelloWorld }
+}
+
+createImageBitmap(() => {
+  console.log('Image clicked')
+})
+
+</script>
